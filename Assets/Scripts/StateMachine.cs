bf169b426cdff0b97d4858bdf8b7f599a007f5ed
xvcrimsajadevx <xvcrimsajadevx@hotmail.com>
2022-05-25 01:45:27 -0600

    State Machine Created

---
+++ Assets/Scripts/StateMachine.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class StateMachine : MonoBehaviour
+{
+    private State currentState;
+
+    public void SwitchState(State newState)
+    {
+        currentState?.Exit();
+
+        currentState = newState;
+        currentState?.Enter();
+    }
+
+    private void Update()
+    {
+        currentState?.Tick(Time.deltaTime);
+    }
+
+    
+}
