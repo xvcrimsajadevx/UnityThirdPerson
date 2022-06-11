32f616ebbff9367eb45bf14caf4cfb08142e6789
xvcrimsajadevx <xvcrimsajadevx@hotmail.com>
2022-06-10 22:27:37 -0600

    Force Receiver - Gravity Added

--- Assets/Scripts/StateMachines/Player/PlayerBaseState.cs
+++ Assets/Scripts/StateMachines/Player/PlayerBaseState.cs
@@ -6,6 +6,11 @@ public abstract class PlayerBaseState : State
 {
     protected PlayerStateMachine stateMachine;
 
+    protected void Move(Vector3 motion, float deltaTime)
+    {
+        stateMachine.Controller.Move((motion + stateMachine.ForceReceiver.Movement) * deltaTime);
+    }
+
     public PlayerBaseState(PlayerStateMachine stateMachine)
     {
         this.stateMachine = stateMachine;
