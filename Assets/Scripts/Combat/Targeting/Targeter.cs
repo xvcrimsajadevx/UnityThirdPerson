d9695115049fe3debbb09b389c232d7b6865b159
xvcrimsajadevx <xvcrimsajadevx@hotmail.com>
2022-06-12 02:51:35 -0600

    Targeting refined, movement added

--- Assets/Scripts/Combat/Targeting/Targeter.cs
+++ Assets/Scripts/Combat/Targeting/Targeter.cs
@@ -7,9 +7,15 @@ public class Targeter : MonoBehaviour
 {
     [SerializeField] private CinemachineTargetGroup cineTargetGroup;
     
+    private Camera mainCamera;
     private List<Target> targets = new List<Target>();
 
     public Target CurrentTarget { get; private set; }
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -32,7 +38,29 @@ public class Targeter : MonoBehaviour
     {
         if (targets.Count == 0) { return false; }
 
-        CurrentTarget = targets[0];
+        Target closestTarget = null;
+        float closestTargetDistance = Mathf.Infinity;
+
+        foreach (Target target in targets)
+        {
+            Vector2 viewPos = mainCamera.WorldToViewportPoint(target.transform.position);
+
+            if (viewPos.x < 0 || viewPos.x > 1 || viewPos.y < 0 || viewPos.y > 1)
+            {
+                continue;
+            }
+
+            Vector2 toCenter = viewPos - new Vector2(0.5f,0.5f);
+            if (toCenter.sqrMagnitude < closestTargetDistance)
+            {
+                closestTarget = target;
+                closestTargetDistance = toCenter.sqrMagnitude;
+            }
+        }
+
+        if (closestTarget == null) { return false; }
+
+        CurrentTarget = closestTarget;
         cineTargetGroup.AddMember(CurrentTarget.transform, 1F, 2F);
 
         return true;
--- Assets/Scripts/StateMachines/Player/PlayerTargetingState.cs
+++ Assets/Scripts/StateMachines/Player/PlayerTargetingState.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerTargetingState : PlayerBaseState
 {
     private readonly int TargetingBlendTreeHash = Animator.StringToHash("TargetingBlendTree");
+    private readonly int TargetingForwardHash = Animator.StringToHash("TargetingForward");
+    private readonly int TargetingRightHash = Animator.StringToHash("TargetingRight");
+
     public PlayerTargetingState(PlayerStateMachine stateMachine) : base(stateMachine) { }
 
     public override void Enter()
@@ -22,6 +25,13 @@ public class PlayerTargetingState : PlayerBaseState
             return;
         }
 
+        Vector3 movement = CalculateMovement();
+        Move(movement * stateMachine.TargetingMovementSpeed, deltaTime);
+
+        UpdateAnimator(deltaTime);
+
+        FaceTarget();
+
         Debug.Log(stateMachine.Targeter.CurrentTarget.name);
     }
 
@@ -35,5 +45,38 @@ public class PlayerTargetingState : PlayerBaseState
         stateMachine.Targeter.Cancel();
 
         stateMachine.SwitchState(new PlayerFreeLookState(stateMachine));
+    }
+
+    private Vector3 CalculateMovement()
+    {
+        Vector3 movement = new Vector3();
+
+        movement += stateMachine.transform.right * stateMachine.InputReader.MovementValue.x;
+        movement += stateMachine.transform.forward * stateMachine.InputReader.MovementValue.y;
+
+        return movement;
+    }
+
+    private void UpdateAnimator(float deltaTime)
+    {
+        if (stateMachine.InputReader.MovementValue.y == 0)
+        {
+            stateMachine.Animator.SetFloat(TargetingForwardHash, 0f, 0.1f, deltaTime);
+        }
+        else
+        {
+            float value = stateMachine.InputReader.MovementValue.y > 0 ? 1f : -1f;
+            stateMachine.Animator.SetFloat(TargetingForwardHash, value, 0.1f, deltaTime);
+        }
+
+        if (stateMachine.InputReader.MovementValue.x == 0)
+        {
+            stateMachine.Animator.SetFloat(TargetingRightHash, 0f, 0.1f, deltaTime);
+        }
+        else
+        {
+            float value = stateMachine.InputReader.MovementValue.x > 0 ? 1f : -1f;
+            stateMachine.Animator.SetFloat(TargetingRightHash, value, 0.1f, deltaTime);
+        }
     }
 }
